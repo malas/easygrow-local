2350ae5a60dd2610111d1b0f0834b61cf04fa839
Laurynas Mališauskas <laurynas.malisauskas@gmail.com>
2016-08-27 18:36:07 +0300

    no express, jog names diferentiated

--- app/index.js
+++ app/index.js
@@ -32,7 +32,12 @@ var agenda = new Agenda({
 	defaultLockLifetime: 30000 // 30 seconds
 });
 
-agenda.define('relay switch', function(job, done) {
+agenda.define('relay switch on', function(job, done) {
+  var data = job.attrs.data;
+  relaySwitch(data.pins, data.status, done);
+});
+
+agenda.define('relay switch off', function(job, done) {
   var data = job.attrs.data;
   relaySwitch(data.pins, data.status, done);
 });
@@ -44,8 +49,8 @@ agenda.define('pwm switch', function(job, done) {
 
 agenda.on('ready', function() {
   // Alternatively, you could also do:
-  agenda.every('0-4,10-14,20-24,30-34,40-44,50-54 * * * *', 'relay switch', { pins: relayPins, status: true });
-  agenda.every('5-9,15-19,25-29,35-39,45-49,55-59 * * * *', 'relay switch', { pins: relayPins, status: false });
+  agenda.every('0-4,10-14,20-24,30-34,40-44,50-54 * * * *', 'relay switch on', { pins: relayPins, status: true });
+  agenda.every('5-9,15-19,25-29,35-39,45-49,55-59 * * * *', 'relay switch off', { pins: relayPins, status: false });
 
   agenda.every('15-40 * * * *', 'pwm switch', { pins: [2], status: 20 });
 
