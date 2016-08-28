86d38b538d7d9c5a09a5d70fff7cb22aee16e6cb
Laurynas Mališauskas <laurynas.malisauskas@gmail.com>
2016-08-28 11:25:23 +0300

    Added PWM pins functionality

--- app/index.js
+++ app/index.js
@@ -15,13 +15,21 @@ function relaySwitch(pins, status, done) {
 
 function pwmSwitch(pins, status, done) {
 	console.log('PWM: ' + status + " on " + pins);
+  for (var i = 0, len = pins.length; i < len; i++) {
+    wpi.pwmWrite(pins[i], status);
+  }
 	done();
 }
 
+// initialization
 wpi.setup('wpi');
 var relayPins = [7];
 for (var i = 0, len = relayPins.length; i < len; i++) {
   wpi.pinMode(relayPins[i], wpi.OUTPUT);
+}
+var pwmPins = [1];
+for (var i = 0, len = pwmPins.length; i < len; i++) {
+  wpi.pinMode(relayPins[i], wpi.PWM_OUTPUT);
 }
 
 var mongoConnectionString = "mongodb://db:27017/agenda";
@@ -32,27 +40,35 @@ var agenda = new Agenda({
 	defaultLockLifetime: 30000 // 30 seconds
 });
 
+// job definitions
 agenda.define('relay switch on', function(job, done) {
   var data = job.attrs.data;
-  relaySwitch(data.pins, data.status, done);
+  relaySwitch(data.pins, true, done);
 });
 
 agenda.define('relay switch off', function(job, done) {
   var data = job.attrs.data;
-  relaySwitch(data.pins, data.status, done);
+  relaySwitch(data.pins, false, done);
 });
 
-agenda.define('pwm switch', function(job, done) {
+agenda.define('pwm switch on', function(job, done) {
   var data = job.attrs.data;
-  pwmSwitch(data.pins, data.status, done);
+  pwmSwitch(data.pins, 1024, done);
+});
+
+agenda.define('pwm switch off', function(job, done) {
+  var data = job.attrs.data;
+  pwmSwitch(data.pins, 0, done);
 });
 
 agenda.on('ready', function() {
-  // Alternatively, you could also do:
+  // initial new jobs saved into db
+  /*
   agenda.every('0-4,10-14,20-24,30-34,40-44,50-54 * * * *', 'relay switch on', { pins: relayPins, status: true });
   agenda.every('5-9,15-19,25-29,35-39,45-49,55-59 * * * *', 'relay switch off', { pins: relayPins, status: false });
-
-  agenda.every('15-40 * * * *', 'pwm switch', { pins: [2], status: 20 });
+  */
+  agenda.every('* 8-20 * * *', 'pwm switch on', { pins: pwmPins });
+  agenda.every('* 0-7,21-23 * * *', 'pwm switch off', { pins: pwmPins });
 
   agenda.start();
 });
@@ -61,4 +77,4 @@ agenda.on('start', function(job) {
   console.log("Job %s starting", job.attrs.name);
 });
 
-console.log("Fully started NodeJS");
+console.log("Fully started Local worker");
